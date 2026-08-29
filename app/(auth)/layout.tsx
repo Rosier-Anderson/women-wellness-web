0a63bac4fec06a582ab69b316dfd5ae54fce9407
Rosier  Anderson <rosieranderson@Rosiers-MacBook-Air.local>
2026-08-29 02:22:05 -0400

    fix: correct footer import path

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/layout/Header";
 import { Container } from "@/components/ui/global/Container";
 
